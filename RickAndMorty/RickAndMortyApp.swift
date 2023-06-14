ad70bc9d53ceeb95d78edbb3ab6976abbaca7128
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-14 18:37:00 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,20 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Samuel Baraka on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
